78a703eb0cff5d392731e91c8465cce4b9f989a8
WeededMuffin385 <zagoruiko.mihail@mail.ru>
2024-05-17 17:33:34 +0300

    Working exercise 3.2

--- section_3/task_2/exercise_2/src/app.rs
+++ section_3/task_2/exercise_2/src/app.rs
@@ -3,7 +3,7 @@ use std::ops::Not;
 use std::process::exit;
 use eframe::Frame;
 use egui::{Context, Ui};
-use egui_plot::{Line, Plot, PlotPoints};
+use egui_plot::{Line, Plot, PlotPoints, Points};
 use nalgebra::{DMatrix, DVector};
 use rand::Rng;
 use crate::model::*;
@@ -14,7 +14,13 @@ pub struct App {
 	random: bool,
 	amount: usize,
 	points: Vec<[f64; 2]>,
+	cubic_spline_coefficients: Vec<f64>,
 	parabolic_spline_coefficients: Vec<f64>,
+
+
+	show_cubic: bool,
+	show_parabolic: bool,
+	first_derivative: f64,
 }
 
 
@@ -25,25 +31,30 @@ impl eframe::App for App {
 
 			ui.horizontal(|ui|{
 				if ui.add(egui::Slider::new(&mut self.amount, 3..=16).text("amount of points")).changed() {
-					if self.random {
-						self.points = generate_random_points(self.amount);
-					} else {
-						self.points = generate_points(self.amount);
-					}
-					self.parabolic_spline_coefficients = generate_parabolic_spline_coefficients(&self.points);
+					self.generate_coefficients();
 				}
 			});
 
-			if ui.button("regenerate").clicked() {
-				if self.random {
-					self.points = generate_random_points(self.amount);
-				} else {
-					self.points = generate_points(self.amount);
+			ui.horizontal(|ui|{
+				if ui.add(egui::Slider::new(&mut self.first_derivative, -5.0..=5.0).text("first derivative")).changed() {
+					self.generate_coefficients();
 				}
-				self.parabolic_spline_coefficients = generate_parabolic_spline_coefficients(&self.points);
+			});
+
+
+			if ui.button("regenerate").clicked() {
+				self.generate_coefficients();
+			}
+
+			if ui.button("use real derivatives").clicked() {
+				self.first_derivative = f_d(self.points[0][0]);
+				self.generate_coefficients();
 			}
 
 			ui.checkbox(&mut self.random, "generate points in random places");
+
+			ui.checkbox(&mut self.show_cubic, "show cubic");
+			ui.checkbox(&mut self.show_parabolic, "show parabolic");
 
 			self.render_plot(ui);
 		});
@@ -55,15 +66,20 @@ impl Default for App {
 		let amount = 4;
 		let random = false;
 		let points = generate_points(amount);
-		let parabolic_spline_coefficients = generate_parabolic_spline_coefficients(&points);
-
-		generate_cubic_spline_coefficients(&points);
+		let cubic_spline_coefficients = generate_cubic_spline_coefficients(&points);
+		let parabolic_spline_coefficients = generate_parabolic_spline_coefficients(&points, 0.0);
 
 		Self {
 			random,
 			amount,
 			points,
+			cubic_spline_coefficients,
 			parabolic_spline_coefficients,
+
+			show_cubic: true,
+			show_parabolic: true,
+
+			first_derivative: 0.0,
 		}
 	}
 }
@@ -74,15 +90,38 @@ impl App {
 			let original_line = self.generate_original_line();
 			plot_ui.line(original_line);
 
-			let parabolic_spline_line = self.generate_parabolic_spline_line();
-			plot_ui.line(parabolic_spline_line);
+			if self.show_parabolic {
+				let parabolic_spline_line = self.generate_parabolic_spline_line();
+				plot_ui.line(parabolic_spline_line);
+			}
+
+			let original_points = self.generate_original_points();
+			plot_ui.points(original_points);
+
+			if self.show_cubic {
+				let cubic_spline_line = self.generate_cubic_spline_line();
+				plot_ui.line(cubic_spline_line);
+			}
 		});
+	}
+
+	fn generate_coefficients(&mut self) {
+		if self.random {
+			self.points = generate_random_points(self.amount);
+		} else {
+			self.points = generate_points(self.amount);
+		}
+		self.cubic_spline_coefficients = generate_cubic_spline_coefficients(&self.points);
+		self.parabolic_spline_coefficients = generate_parabolic_spline_coefficients(&self.points, self.first_derivative);
 	}
 
 	fn generate_original_line(&self) -> Line {
 		Line::new(PlotPoints::new(self.points.to_vec()))
 	}
 
+	fn generate_original_points(&self) -> Points {
+		Points::new(self.points.clone()).radius(6.0)
+	}
 
 	fn parabolic_spline(&self, x: f64) -> f64 {
 		for (index, points) in self.points.windows(2).enumerate() {
@@ -92,6 +131,20 @@ impl App {
 			if (x >= left) && (x <= right) {
 				let &[a, b, c] = &self.parabolic_spline_coefficients[index * 3 .. (index + 1) * 3] else {panic!()};
 				return a * x.powi(2) + b * x + c;
+			}
+		}
+
+		0.0
+	}
+
+	fn cubic_spline(&self, x: f64) -> f64 {
+		for (index, points) in self.points.windows(2).enumerate() {
+			let left = points[0][0];
+			let right = points[1][0];
+
+			if (x >= left) && (x <= right) {
+				let &[a, b, c, d] = &self.cubic_spline_coefficients[index * 4 .. (index + 1) * 4] else {panic!()};
+				return a * x.powi(3) + b * x.powi(2) + c * x + d;
 			}
 		}
 
@@ -113,6 +166,22 @@ impl App {
 			[x, y]
 		}).collect()))
 	}
+
+	fn generate_cubic_spline_line(&self) -> Line {
+		let points_amount = 5000;
+
+		let begin = self.points.first().unwrap()[0];
+		let end = self.points.last().unwrap()[0];
+
+		let delta = end - begin;
+		let step = delta / (points_amount - 1) as f64;
+
+		Line::new(PlotPoints::new((0..points_amount).map(|index| {
+			let x = (index as f64) * step + begin;
+			let y = self.cubic_spline(x);
+			[x, y]
+		}).collect()))
+	}
 }
 
 fn generate_points(amount: usize) -> Vec<[f64; 2]> {
@@ -129,7 +198,7 @@ fn generate_points(amount: usize) -> Vec<[f64; 2]> {
 fn generate_random_points(amount: usize) -> Vec<[f64; 2]> {
 	let mut rng = rand::thread_rng();
 
-	let mut points: Vec<_> = (0..amount).map(|index| {
+	let mut points: Vec<_> = (0..amount - 2).map(|index| {
 		let x = rng.gen_range(A..B);
 		let y = f(x);
 		[x, y]
@@ -141,7 +210,7 @@ fn generate_random_points(amount: usize) -> Vec<[f64; 2]> {
 	points
 }
 
-fn generate_parabolic_spline_coefficients(points: &[[f64; 2]]) -> Vec<f64> {
+fn generate_parabolic_spline_coefficients(points: &[[f64; 2]], first_derivative: f64) -> Vec<f64> {
 	let mut variables_matrix = Vec::default();
 	let mut free_matrix = Vec::default();
 
@@ -166,7 +235,7 @@ fn generate_parabolic_spline_coefficients(points: &[[f64; 2]]) -> Vec<f64> {
 	{
 		let [x, y] = points[0];
 		variables_matrix.extend_from_slice(&vec![vec![2.0 * x, 1.0, 0.0], vec![0.0; (num_unknown_variables - 1) * 3]].concat());
-		free_matrix.extend_from_slice(&vec![0.0]);
+		free_matrix.extend_from_slice(&vec![first_derivative]);
 	}
 
 
@@ -180,60 +249,47 @@ fn generate_parabolic_spline_coefficients(points: &[[f64; 2]]) -> Vec<f64> {
 fn generate_cubic_spline_coefficients(points: &[[f64; 2]]) -> Vec<f64> {
 	let mut variables_matrix = Vec::default();
 	let mut free_matrix = Vec::default();
-
 	let num_unknown_variables = points.windows(2).len();
 
-	{ // values of functions
-		let variables: Vec<_> = points.windows(2).enumerate().map(|(index, points)|{
-			let h = points[1][0] - points[0][0];
-			vec![vec![0.0; index * 4], vec![h.powi(3), h.powi(2), h, 1.0], vec![0.0; (num_unknown_variables - index - 1) * 4]].concat()
-		}).collect();
-		let free: Vec<_> = points[1..points.len()].iter().map(|&[_, y]| y).collect();
-
-		println!("{:?}", variables);
-		println!("{:?}", free);
+	for (index, points) in points.windows(2).enumerate() {
+		let variables: Vec<_> = points.iter().map(|&[x, y]| vec![vec![0.0; index * 4], vec![x.powi(3), x.powi(2), x, 1.0], vec![0.0; (num_unknown_variables - index - 1) * 4]].concat()).collect();
+		let free: Vec<_> = points.iter().map(|&[x, y]| y).collect();
 
 		variables_matrix.extend_from_slice(&variables.concat());
 		free_matrix.extend_from_slice(&free);
 	}
 
-	println!("[DERIVATIVES OF 1ST ORDER]\n\n");
+	for (index, &[x, y]) in points[1..points.len() - 1].iter().enumerate() {
+		let variables: Vec<_> = vec![vec![0.0; index * 4], vec![3.0 * x.powi(2), 2.0 * x, 1.0, 0.0], vec![-3.0 * x.powi(2), -2.0 * x, -1.0, 0.0], vec![0.0; (num_unknown_variables - index - 2) * 4]].concat();
+		let free: Vec<_> = vec![0.0];
 
-
-	{ // derivatives of 1-st order
-		let variables: Vec<_> = points.windows(2).enumerate().map(|(index, points)|{
-			let h = points[1][0] - points[0][0];
-			vec![vec![0.0; index * 4], vec![3.0 * h.powi(2), 2.0 * h, 1.0, 0.0], vec![-3.0 * h.powi(2), -2.0 * h, -1.0, 0.0], vec![0.0; (num_unknown_variables - index - 1) * 4]].concat()
-		}).collect();
-		let free = vec![0.0; variables.len()];
-
-		println!("{:?}", variables);
-		println!("{:?}", free);
+		variables_matrix.extend_from_slice(&variables);
+		free_matrix.extend_from_slice(&free);
 	}
 
-	exit(0);
+	for (index, &[x, y]) in points[1..points.len() - 1].iter().enumerate() {
+		let variables: Vec<_> = vec![vec![0.0; index * 4], vec![3.0 * 2.0 * x, 2.0, 0.0, 0.0], vec![-3.0 * 2.0 * x, -2.0, 0.0, 0.0], vec![0.0; (num_unknown_variables - index - 2) * 4]].concat();
+		let free: Vec<_> = vec![0.0];
 
-	{ // derivatives of 2-nd order
-		let variables: Vec<_> = points.windows(2).enumerate().map(|(index, points)|{
-			let delta = points[1][0] - points[0][0];
-			vec![vec![0.0; index * 4], vec![3.0 * 2.0 * delta.powi(1), 2.0, 0.0, 0.0], vec![0.0; (num_unknown_variables - index - 1) * 4]].concat()
-		}).collect();
+		variables_matrix.extend_from_slice(&variables);
+		free_matrix.extend_from_slice(&free);
 	}
 
-	exit(0);
-
-
-	for (index, points) in points.windows(2).enumerate() {
-		// let variables: Vec<_> = points.iter().map()
-
-
-		//let variables: Vec<_> = points.iter().map(|&[x, y]| vec![vec![0.0; index * 3], vec![x.powi(2), x, 1.0], vec![0.0; (num_unknown_variables - index - 1) * 3]].concat()).collect();
-		//let free: Vec<_> = points.iter().map(|&[x, y]| y).collect();
-
-		//variables_matrix.extend_from_slice(&variables.concat());
-		//free_matrix.extend_from_slice(&free);
+	{
+		let &[x, y] = points.first().unwrap();
+		variables_matrix.extend_from_slice(&vec![vec![3.0 * 2.0 * x, 2.0, 0.0, 0.0], vec![0.0; (num_unknown_variables - 1) * 4]].concat());
+		free_matrix.extend_from_slice(&vec![0.0]);
 	}
 
+	{
+		let &[x, y] = points.last().unwrap();
+		variables_matrix.extend_from_slice(&vec![vec![0.0; (num_unknown_variables - 1) * 4], vec![3.0 * 2.0 * x, 2.0, 0.0, 0.0]].concat());
+		free_matrix.extend_from_slice(&vec![0.0]);
+	}
 
-	vec![]
+	let variables_matrix = DMatrix::from_vec(num_unknown_variables * 4, num_unknown_variables * 4, variables_matrix).transpose();
+	let free_matrix = DVector::from_vec(free_matrix);
+	let result = variables_matrix.try_inverse().unwrap() * free_matrix;
+
+	result.as_slice().to_vec()
 }
--- section_3/task_2/exercise_2/src/model.rs
+++ section_3/task_2/exercise_2/src/model.rs
@@ -1,6 +1,33 @@
+
+
 pub const A: f64 = 0.5;
 pub const B: f64 = 1.5;
 
 pub fn f(x: f64) -> f64 {
 	2.0 * x.powi(2) - 5.0 * x + x.powi(2).sin() * x.atan()
 }
+
+pub fn f_d(x: f64) -> f64 {
+	4.0 * x - 5.0 + 2.0 * x * x.powi(2).cos() * x.atan() + x.powi(2) / (x.powi(2) + 1.0)
+}
+
+pub fn f_dd(x: f64) -> f64 {
+	4.0 + 2.0 * x.powi(2).cos() * x.atan() - 4.0 * x.powi(2) * x.powi(2).sin() * x.atan() + 4.0 * x * x.powi(2).cos() / (x.powi(2) + 1.0) - 2.0 * x.powi(2).sin() * x / (x.powi(2) + 1.0).powi(2)
+}
+
+
+/*
+pub const A: f64 = 1.0;
+pub const B: f64 = 2.2;
+
+pub fn f(x: f64) -> f64 {
+	(1.0-x).exp() + x.powi(2).cos()
+
+	// 2.0 * x.powi(2) - 5.0 * x + x.powi(2).sin() * x.atan()
+}
+
+//pub fn f_derivative(x: f64) -> f64 {
+
+//}
+
+ */
